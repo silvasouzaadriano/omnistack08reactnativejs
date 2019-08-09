dcdf0c9bc003b98ed37f990ddbaaf0992d4bdeea
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-09 01:34:25 -0300

    Routes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   KeyboardAvoidingView,
   Platform,
@@ -6,16 +6,20 @@ import {
   StyleSheet,
   Image,
   TextInput,
-  TouchableOpacity
-} from "react-native";
+  TouchableOpacity,
+} from 'react-native';
 
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
-export default function Login() {
+export default function Login({ navigation }) {
+  function handleLogin() {
+    navigation.navigate('Main');
+  }
+
   return (
     <KeyboardAvoidingView
       behavior="padding"
-      enabled={Platform.OS === "ios"}
+      enabled={Platform.OS === 'ios'}
       style={styles.container}
     >
       <Image source={logo} />
@@ -26,7 +30,7 @@ export default function Login() {
         placeholderTextColor="#999"
         style={styles.input}
       />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text style={styles.buttonText}>Enviar</Text>
       </TouchableOpacity>
     </KeyboardAvoidingView>
@@ -36,36 +40,36 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 30,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 30
   },
 
   input: {
     height: 46,
-    alignSelf: 'stretch',
-    backgroundColor: '#fff',
+    alignSelf: "stretch",
+    backgroundColor: "#fff",
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderRadius: 4,
     marginTop: 20,
-    paddingHorizontal: 15,
+    paddingHorizontal: 15
   },
 
   button: {
     height: 46,
-    alignSelf: "stretch",
-    backgroundColor: "#df4723",
+    alignSelf: 'stretch',
+    backgroundColor: '#df4723',
     borderRadius: 4,
     marginTop: 10,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
-  }
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16
+  },
 });
