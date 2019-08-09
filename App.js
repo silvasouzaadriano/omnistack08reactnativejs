17b7759f9148b94661842d7496d72531c119e0e7
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-08 23:31:01 -0300

    First StyleSheet setup

--- App.js
+++ App.js
@@ -6,13 +6,28 @@
  * @flow
  */
 
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#7159c1' }}>
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  text: {
+    fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 20
+  },
+});
