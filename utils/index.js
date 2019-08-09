c35b63b7e7e5aa907dc702d5287ecd9ae0586998
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-08 22:52:34 -0300

    Initial version

---
+++ utils/index.js
@@ -0,0 +1,32 @@
+// Comando para criar o projeto 
+Forma mais usada: npx react-native init <nome do projet>
+Forma alternativa: (https://yarnpkg.com/lang/en/docs/cli/global/)
+Essa opção precisa configurar a variável global yarn no path
+1) yarn global add react-native-cli e depois rodar
+2) react-native init 
+
+Informações úteis após criar o projeto:
+Run instructions for iOS:
+    • cd tindev && react-native run-ios
+    - or -
+    • Open tindev/ios/tindev.xcodeproj in Xcode or run "xed -b ios"
+    • Hit the Run button
+
+  Run instructions for Android:
+    • Have an Android emulator running (quickest way to get started), or a device connected.
+    • cd tindev && react-native run-android
+
+
+// Instalar emulador mobile
+1) Acessar o endereço: https://docs.rocketseat.dev e seguir as instruçõs de acordo com a plataforma
+Pasta Android: /home/adriano/Android/Sdk
+
+2) yarn react-native run-android
+
+3) Abrir android studio:
+
+cd /opt/android-studio/bin/
+
+./studio.sh
+
+
